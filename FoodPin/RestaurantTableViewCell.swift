121afe8062e7d912f32f77b003c1275e9a292ef5
Li He <heil.vip.cn@gmail.com>
2025-11-29 21:25:17 +0800

    自定义accessoryView, 针对iPad优化

--- FoodPin/RestaurantTableViewCell.swift
+++ FoodPin/RestaurantTableViewCell.swift
@@ -14,12 +14,15 @@ class RestaurantTableViewCell: UITableViewCell {
     @IBOutlet var thumbnailImageView: UIImageView! {
         didSet {
             thumbnailImageView.layer.cornerRadius = 10
+            thumbnailImageView.clipsToBounds = true
         }
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.tintColor = .systemYellow//更改对勾颜色
+        thumbnailImageView.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -27,5 +30,21 @@ class RestaurantTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configureCustomAccessory() {
+            
+            // 1. 创建自定义视图（例如 UIImageView 或 UIButton）
+            let customImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
+            
+            // 2. 配置自定义视图的外观
+            if let customImage = UIImage(systemName: "heart.fill") {
+                customImageView.image = customImage
+                customImageView.tintColor = .systemRed // 设置颜色
+                customImageView.contentMode = .scaleAspectFit
+            }
+            
+            // 3. 将自定义视图赋值给 accessoryView
+            self.accessoryView = customImageView
+        }
 
 }
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -13,11 +13,14 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
     var restaurantType = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
-
+    
+    var restaurantIsFavorites = Array(repeating: false, count: 21)
+    
     lazy var dataSource = configureDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.cellLayoutMarginsFollowReadableWidth = true//自动调节cell宽度
 
         tableView.dataSource = dataSource
         tableView.separatorStyle = .none
@@ -31,15 +34,66 @@ class RestaurantTableViewController: UITableViewController {
     
     func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
         let dataSource = UITableViewDiffableDataSource<Section, String>(tableView: tableView) { tableView, indexPath, resturantName in
-            let cell = tableView.dequeueReusableCell(withIdentifier: "favoritecell", for: indexPath) as! RestaurantTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "datacell", for: indexPath) as! RestaurantTableViewCell
             cell.nameLabel.text = resturantName
             cell.typeLabel.text = self.restaurantType[indexPath.row]
             cell.locationLabel.text = self.restaurantLocations[indexPath.row]
             cell.thumbnailImageView.image = UIImage(named: resturantName)
+            if self.restaurantIsFavorites[indexPath.row] {
+                cell.configureCustomAccessory()
+            } else {
+                cell.accessoryType = .none
+                cell.accessoryView = nil
+            }
             
             return cell
         }
         return dataSource
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        //针对iPad
+        if let popoverController = optionMenu.popoverPresentationController {
+            if let cell = tableView.cellForRow(at: indexPath) {
+                popoverController.sourceView = cell
+                popoverController.sourceRect = cell.bounds
+            }
+        }
+        
+        //reserve选项
+        let reserveActionHandler = { (action: UIAlertAction) -> Void in
+            let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry, this feature is not yet available yet. Please retry later", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+            
+        }
+        let reserveAction = UIAlertAction(title: "Reserve a table", style: .default, handler: reserveActionHandler)
+        optionMenu.addAction(reserveAction)
+        
+        //favorite选项
+        let title = self.restaurantIsFavorites[indexPath.row] ? "Undo Checkin" : "Mark as favorite"
+        let favoriteAction = UIAlertAction(title: title, style: .default) { (action: UIAlertAction) -> Void in
+            self.restaurantIsFavorites[indexPath.row] = !self.restaurantIsFavorites[indexPath.row]
+            let cell = self.tableView.cellForRow(at: indexPath)! as! RestaurantTableViewCell
+            if self.restaurantIsFavorites[indexPath.row] {
+                cell.configureCustomAccessory()
+            } else {
+                cell.accessoryView = .none
+                cell.accessoryView = nil
+            }
+        }
+        optionMenu.addAction(favoriteAction)
+        
+        //cancel选项
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        present(optionMenu, animated: true, completion: nil)
+        
+        //取消选择
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
 
