8815533e98f19f49511d6858ea026124a28c4168
Li He <heil.vip.cn@gmail.com>
2025-12-02 10:10:09 +0800

    详情页显示更多信息; UI调整

---
+++ FoodPin/View/RestaurantDetailTwoColumnCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantDetailTwoColumnCell.swift
+//  FoodPin
+//
+//  Created by  He on 2025/12/1.
+//
+
+import UIKit
+
+class RestaurantDetailTwoColumnCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
